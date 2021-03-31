9262efa6f02caa8a9aa62a927c7db24b8db970bd
ProgrammingWithSoumyansh <programmingwithsoumyansh@gmail.com>
2021-03-31 14:54:10 +0530

    Heroku Deployment

--- flaskBlog/__init__.py
+++ flaskBlog/__init__.py
@@ -2,10 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+from config import Config
 app = Flask(__name__)
-app.config['SECRET_KEY']='mysecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///database.db'
+app.config.from_object(Config)
 db= SQLAlchemy(app)
 bcrypt=Bcrypt(app)
 login_manager = LoginManager(app)
